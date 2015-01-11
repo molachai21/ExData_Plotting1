cff95c2c86f384b264e23d4e1799d4c4129f3da0
molachai21 <appel.andy@gmail.com>
2015-01-11 13:07:52 -0800

    Uploading Course Project 1
    Contains R and png files

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+globalactivepower <- as.numeric(subSetData$Global_active_power)
+png(filename = "plot1.png", width = 480, height = 480)
+hist(globalactivepower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
